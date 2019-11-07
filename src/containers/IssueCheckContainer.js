ce45d0648598f7384d44a54f0f87210d5728c82d
Martin Lundfall <martin.lundfall@protonmail.com>
2019-11-07 16:07:59 +0100

    plugging some things in

--- src/containers/IssueCheckContainer.js
+++ src/containers/IssueCheckContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Web3 from "web3";
+import Web3 from 'web3';
+import daiABI from '../utils/daiABI.json';
+import dachABI from '../utils/dachABI.json';
 import { withStore } from '@spyna/react-store'
 import { withStyles } from '@material-ui/styles';
 import theme from '../theme/theme'
 import classNames from 'classnames'
+import { signCheque } from '../utils/walletUtils'
 
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import WifiIcon from '@material-ui/icons/Wifi';
@@ -20,225 +23,239 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 
 
 const styles = () => ({
-    container: {
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(3),
-        minHeight: 52
-    },
-    actionsContainer: {
-        marginTop: theme.spacing(3)
-    },
-    accountButton: {
-      '& svg': {
-        marginRight: theme.spacing(1)
-      }
-    },
+  container: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(3),
+    minHeight: 52
+  },
+  actionsContainer: {
+    marginTop: theme.spacing(3)
+  },
+  accountButton: {
+    '& svg': {
+      marginRight: theme.spacing(1)
+    }
+  },
   input: {
-      width: '100%',
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(3)
+    width: '100%',
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(3)
   },
   account: {
   },
   panel: {
-      padding: theme.spacing(3),
-      border: '1px solid #eee',
-      borderRadius: theme.spacing(1)
+    padding: theme.spacing(3),
+    border: '1px solid #eee',
+    borderRadius: theme.spacing(1)
   },
   accountItem: {
-      marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(2)
   },
   permit: {
-      marginTop: 14
+    marginTop: 14
   },
   permitButton: {
-      [theme.breakpoints.down('sm')]: {
-          marginTop: theme.spacing(1)
-      },
-      [theme.breakpoints.up('md')]: {
-          textAlign: 'right'
-      },
+    [theme.breakpoints.down('sm')]: {
+      marginTop: theme.spacing(1)
+    },
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'right'
+    },
   }
 })
 
 class IssueCheckContainer extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    async componentDidMount() {
-    }
+  async componentDidMount() {
+  }
 
-    async initBrowserWallet() {
-        const store = this.props.store
+  async initBrowserWallet() {
+    const store = this.props.store
 
-        store.set('walletLoading', true)
+    store.set('walletLoading', true)
 
-        let web3Provider;
+    let web3Provider;
 
-        // Initialize web3 (https://medium.com/coinmonks/web3-js-ethereum-javascript-api-72f7b22e2f0a)
-        // Modern dApp browsers...
-        if (window.ethereum) {
-            web3Provider = window.ethereum;
-            try {
-                // Request account access
-                await window.ethereum.enable();
-            } catch (error) {
-                // User denied account access...
-                console.error("User denied account access")
-            }
-        }
-        // Legacy dApp browsers...
-        else if (window.web3) {
-            web3Provider = window.web3.currentProvider;
-        }
-        // If no injected web3 instance is detected, fall back to Ganache
-        else {
-            this.log("Please install MetaMask!");
-        }
-
-        const web3 = new Web3(web3Provider);
-        const walletType = 'browser'
-        const accounts = await web3.eth.getAccounts()
-
+    // Initialize web3 (https://medium.com/coinmonks/web3-js-ethereum-javascript-api-72f7b22e2f0a)
+    // Modern dApp browsers...
+    if (window.ethereum) {
+      web3Provider = window.ethereum;
+      try {
+        // Request account access
         await window.ethereum.enable();
-
-        store.set('walletLoading', false)
-        store.set('walletAddress', accounts[0])
-        store.set('web3', web3)
-        store.set('walletType', walletType)
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dApp browsers...
+    else if (window.web3) {
+      web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, give some advice
+    else {
+      console.log("Please install MetaMask to continue!");
     }
 
-    render() {
-        const {
-            classes,
-            store
-        } = this.props
+    const web3 = new Web3(web3Provider);
+    const walletType = 'browser';
+    const accounts = await web3.eth.getAccounts();
+    const daiAddress = "0xaaeb56503ceb8852f802bdf050b8ff7d567716ed";
+    const dachAddress = '0xc2433f48f1db3b5067dc412d403b57a3077a52c0';
+    const dai = new web3.eth.Contract(daiABI, daiAddress);
+    const dach = new web3.eth.Contract(dachABI, dachAddress);
+    await window.ethereum.enable();
+    store.set('daiAddress', daiAddress)
+    store.set('walletLoading', false)
+    store.set('walletAddress', accounts[0])
+    store.set('walletType', walletType)
+    store.set('daiObject', dai)
+    // dai.balanceOf(accounts[0]).then((daiBal) => {
+    //   store.set('daiBalance', daiBal);
+    // })
+    
+    // this.log('daibalance')
+    // this.log(daiBalance)
+  }
 
-        const walletAddress = store.get('walletAddress')
-        const daiBalance = store.get('daiBalance')
-        const daiSupply = store.get('daiSupply')
-        const dachAllowance = store.get('dachAllowance')
-        const isSignedIn = walletAddress && walletAddress.length
+  render() {
+    const {
+      classes,
+      store
+    } = this.props
 
-        console.log(this.props, this.state, this.props.store.getState())
+    const walletAddress = store.get('walletAddress')
+    const daiBalance = store.get('daiBalance')
+    const daiSupply = store.get('daiSupply')
+    const dachApproved = store.get('dachApproved')
+    const dachAllowance = store.get('dachAllowance');
+    const isSignedIn = walletAddress && walletAddress.length
+    // const dai = store.get('daiObject')
+    // console.log('daiObject')
+    // console.log(dai)
+    console.log(this.props, this.state, this.props.store.getState())
 
-        return <Grid item xs={12}>
-            {<Grid className={classes.container} container alignItems='center'>
-                <Typography variant='h6'>Welcome to Stablecoin.services</Typography>
-                <Typography variant='subtitle1'>The new version of the Dai contract allows for transfers via signatures, allowing you to send Dai without holding any eth.</Typography>
+    return <Grid item xs={12}>
+      {<Grid className={classes.container} container alignItems='center'>
+       <Typography variant='h6'>Welcome to Stablecoin.services</Typography>
+       <Typography variant='subtitle1'>The new version of the Dai contract allows for transfers via signatures, allowing you to send Dai without holding any eth.</Typography>
 
-                <Grid className={classes.actionsContainer} spacing={4} container>
+       <Grid className={classes.actionsContainer} spacing={4} container>
 
-                    <Grid item sm={12} md={6} className={classes.account}>
-                        <div className={classes.panel}>
-                            <Typography variant='h6'>Your Account</Typography>
-                            <Typography variant='subtitle1'>{walletAddress || '–'}</Typography>
-                            <br />
-                            <Grid container>
-                                <Grid item xs={12} className={classes.accountItem}>
-                                    <Typography variant='subtitle2'>DAI Balance</Typography>
-                                    <Typography variant='subtitle2'>{daiBalance ? (daiBalance + ' DAI')  : '–'}</Typography>
-                                </Grid>
-                                <Grid item xs={12} className={classes.accountItem}>
-                                    <Typography variant='subtitle2'>Total DAI Supply</Typography>
-                                    <Typography variant='subtitle2'>{daiSupply ? (daiSupply + ' DAI')  : '–'}</Typography>
-                                </Grid>
-                                <Grid item xs={12} className={classes.accountItem}>
-                                    <Grid container>
-                                        <Grid item xs={8}>
-                                            <Typography variant='subtitle2'>DACH Allowance</Typography>
-                                            <Typography variant='subtitle2'>{dachAllowance ? (dachAllowance + ' DAI')  : '–'}</Typography>
-                                        </Grid>
-                                        {isSignedIn && <Grid item md={4} className={classes.permitButton}>
-                                            <Button
-                                                color='primary'
-                                                size={'small'}
-                                                onClick={() => {}}
-                                                variant="contained">
-                                             Authorize
-                                            </Button>
-                                        </Grid>}
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                            <div className={classes.permit}>
-                                <Typography variant='subtitle2'>Permit Address</Typography>
-                                <TextField
-                                    placeholder='0x'
-                                    className={classes.input}
-                                    margin="normal"
-                                    variant="outlined"
-                                    onChange={() => { }}
-                                />
-                            </div>
-                            <Button
-                                color='primary'
-                                disabled={!isSignedIn || !dachAllowance}
-                                onClick={() => {}}
-                                variant="contained"
-                                className={classes.permitButton}>
-                              Permit
-                            </Button>
-                        </div>
-                    </Grid>
-                    <Grid item sm={12} md={6}>
-                        <div className={classes.panel}>
-                            <Typography variant='h6'>Issue cheque</Typography>
-                            <br/>
-                            <div>
-                                <Typography variant='subtitle2'>To</Typography>
-                                <TextField
-                                    placeholder='0x'
-                                    className={classes.input}
-                                    margin="normal"
-                                    variant="outlined"
-                                    onChange={() => { }}
-                                />
-                            </div>
-                            <div>
-                                <Typography variant='subtitle2'>Token Amount</Typography>
-                                <TextField
-                                    placeholder='4700'
-                                    className={classes.input}
-                                    margin="normal"
-                                    variant="outlined"
-                                    onChange={() => { }}
-                                    InputProps={{
-                                        endAdornment: <InputAdornment className={classes.endAdornment} position="end">DAI</InputAdornment>
-                                    }}
-                                    inputProps={{ 'aria-label': 'bare' }}
-                                />
-                            </div>
-                            <div>
-                                <Typography variant='subtitle2'>Clearing Fee</Typography>
-                                <TextField
-                                    placeholder='1'
-                                    className={classes.input}
-                                    margin="normal"
-                                    variant="outlined"
-                                    onChange={() => { }}
-                                    InputProps={{
-                                        endAdornment: <InputAdornment className={classes.endAdornment} position="end">DAI</InputAdornment>
-                                    }}
-                                    inputProps={{ 'aria-label': 'bare' }}
-                                />
-                            </div>
-                            <Button
-                                color='primary'
-                                onClick={() => {}}
-                                variant="contained"
-                                disabled={!isSignedIn || !dachAllowance}
-                                className={classes.permitButton}>
-                             Transfer
-                            </Button>
-                        </div>
-                    </Grid>
-                </Grid>
-            </Grid>}
-        </Grid>
-    }
+       <Grid item sm={12} md={6} className={classes.account}>
+       <div className={classes.panel}>
+       <Typography variant='h6'>Your Account</Typography>
+       <Typography variant='subtitle1'>{walletAddress || '–'}</Typography>
+       <br />
+       <Grid container>
+       <Grid item xs={12} className={classes.accountItem}>
+       <Typography variant='subtitle2'>DAI Balance</Typography>
+       <Typography variant='subtitle2'>{daiBalance ? (daiBalance + ' DAI')  : '–'}</Typography>
+       </Grid>
+       <Grid item xs={12} className={classes.accountItem}>
+       <Grid container>
+       <Grid item xs={8}>
+       <Typography variant='subtitle2'>Dai Automated Clearing House</Typography>
+       </Grid>
+       {<Grid item md={4} className={classes.permitButton}>
+        <Button
+        color='primary'
+        size={'small'}
+        onClick={() => {}}
+        disabled={!isSignedIn || dachApproved}
+        variant="contained">
+        {dachApproved ? "Authorized" : "Authorize"}
+        </Button>
+        </Grid>}
+       </Grid>
+       </Grid>
+       </Grid>
+       <div className={classes.permit}>
+       <Typography variant='subtitle2'>Permit Address</Typography>
+       <TextField
+       placeholder='0x'
+       className={classes.input}
+       margin="normal"
+       variant="outlined"
+       onChange={() => { }}
+       />
+       </div>
+       <Button
+       color='primary'
+       disabled={!isSignedIn || !dachApproved}
+       onClick={() => {}}
+       variant="contained"
+       className={classes.permitButton}>
+       Permit
+       </Button>
+       </div>
+       </Grid>
+       <Grid item sm={12} md={6}>
+       <div className={classes.panel}>
+       <Typography variant='h6'>Issue cheque</Typography>
+       <br/>
+       <div>
+       <Typography variant='subtitle2'>To</Typography>
+       <TextField
+       placeholder='0x'
+       className={classes.input}
+       margin="normal"
+       variant="outlined"
+       onChange={(event) => {
+         store.set(event.target.value, 'cheque.to')
+       }}
+       />
+       </div>
+       <div>
+       <Typography variant='subtitle2'>Token Amount</Typography>
+       <TextField
+       placeholder='4700'
+       className={classes.input}
+       margin="normal"
+       variant="outlined"
+       onChange={(event) => {
+         store.set(event.target.value, 'cheque.amount')
+       }}
+       InputProps={{
+         endAdornment: <InputAdornment className={classes.endAdornment} position="end">DAI</InputAdornment>
+       }}
+       inputProps={{ 'aria-label': 'bare' }}
+       />
+       </div>
+       <div>
+       <Typography variant='subtitle2'>Clearing Fee</Typography>
+       <TextField
+       placeholder='1'
+       className={classes.input}
+       margin="normal"
+       variant="outlined"
+       onChange={(event) => {
+         store.set(event.target.value, 'cheque.fee')
+       }}
+       InputProps={{
+         endAdornment: <InputAdornment className={classes.endAdornment} position="end">DAI</InputAdornment>
+       }}
+       inputProps={{ 'aria-label': 'bare' }}
+       />
+       </div>
+       <Button
+       color='primary'
+       onClick={() => {signCheque.bind(this)}}
+       variant="contained"
+       disabled={!isSignedIn || !dachApproved}
+       className={classes.permitButton}>
+       Transfer
+       </Button>
+       </div>
+       </Grid>
+       </Grid>
+       </Grid>}
+    </Grid>
+  }
 }
 
 export default withStyles(styles)(withStore(IssueCheckContainer))
--- src/utils/walletUtils.js
+++ src/utils/walletUtils.js
@@ -1,4 +1,9 @@
 import Web3 from "web3";
+import daiABI from '../utils/daiABI.json';
+import dachABI from '../utils/dachABI.json';
+
+const daiAddress = "0xaaeb56503ceb8852f802bdf050b8ff7d567716ed";
+const dachAddress = '0xc2433f48f1db3b5067dc412d403b57a3077a52c0';
 
 export const initBrowserWallet = async function () {
     const store = this.props.store
@@ -23,7 +28,7 @@ export const initBrowserWallet = async function () {
     else if (window.web3) {
         web3Provider = window.web3.currentProvider;
     }
-    // If no injected web3 instance is detected, fall back to Ganache
+    // If no injected web3 instance is detected, display err
     else {
         this.log("Please install MetaMask!");
     }
@@ -33,13 +38,84 @@ export const initBrowserWallet = async function () {
     const accounts = await web3.eth.getAccounts()
 
     await window.ethereum.enable();
-
+    const BN = web3.utils.BN;
     store.set('walletLoading', false)
     store.set('walletAddress', accounts[0])
     store.set('web3', web3)
     store.set('walletType', walletType)
+    const dai = new web3.eth.Contract(daiABI, daiAddress);
+    const dach = new web3.eth.Contract(dachABI, dachAddress);
+    const daiBalanceRaw = await dai.methods.balanceOf(accounts[0]).call();
+    const daiBalance = parseFloat(web3.utils.fromWei(daiBalanceRaw)).toFixed(2);
+    
+    const dachAllowance = await dai.methods.allowance(accounts[0], dachAddress).call();
+    const dachNonce = await dach.methods.nonces(accounts[0]).call();
+    const dachApproved = dachAllowance == "115792089237316195423570985008687907853269984665640564039457584007913129639935" ;
+    store.set('daiObject', dai)
+    store.set('daiBalance', daiBalance)
+    store.set('dachApproved', dachApproved)
+    store.set('dachAllowance', dachAllowance)
+    store.set('dachNonce', dachNonce)
+}
+
+export const signCheque = async function () {
+  const store = this.props.store
+  const web3 = store.get('web3')
+  const nonce = store.get('dachNonce')
+  const accounts = store.get('accounts')
+  const to = store.get('cheque.to')
+  const amount = store.get('cheque.amount')
+  const fee = store.get('cheque.fee')
+
+  const typedData = JSON.stringify({
+    types: {
+      EIP712Domain: [
+        { name: 'name', type: 'string' },
+        { name: 'version', type: 'string' },
+        { name: 'chainId', type: 'uint256' },
+        { name: 'verifyingContract', type: 'address' },
+      ],
+      Cheque: [
+        { name: 'sender', type: 'address' },
+        { name: 'receiver', type: 'address' },
+        { name: 'amount', type: 'uint256' },
+        { name: 'fee', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'expiry', type: 'uint256' }
+      ],
+    },
+    primaryType: 'Cheque',
+    domain: {
+      name: 'Dai Automated Clearing House',
+      version: '1',
+      chainId: Number(web3.currentProvider.networkVersion),
+      verifyingContract: dachAddress,
+    },
+    message: {
+      sender: accounts[0],
+      receiver: to,
+      amount: amount,
+      fee: fee,
+      nonce: nonce,
+      expiry: 0
+    },
+  });
+
+  //TODO: WHY DOESN"T THIS WORK
+  web3.currentProvider.sendAsync(
+    {
+      method: "eth_signTypedData_v3",
+      params: [accounts[0], typedData],
+      from: accounts[0]
+    },
+    function(err, result) {
+      store.set(err, 'err')
+      store.set(result, 'result')
+    }
+  );
 }
 
 export default {
-    initBrowserWallet
+  initBrowserWallet,
+  signCheque
 }
